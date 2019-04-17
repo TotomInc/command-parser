c1b32f5ce8097e3597a42664358c5ca94d7ad48d
Thomas Cazade <cazade.thomas@gmail.com>
2019-04-17 11:30:20 +0200

    feat: export interfaces

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
 import parser from './parser';
 import autocompleteArgumentValue from './argument/autocomplete-argument-value';
 import { setCommands, getCommands } from './command/commands';
+import { Command, CommandArgument, ParsedArgument } from './models/command.model';
 
-export { parser, autocompleteArgumentValue, setCommands, getCommands };
+export {
+  parser, autocompleteArgumentValue, setCommands, getCommands,
+  Command, CommandArgument, ParsedArgument,
+};
 export default parser;
