9a7bc8243a3765556dd2662329361a6ac76584f0
Thomas Cazade <cazade.thomas@gmail.com>
2019-04-23 17:22:54 +0200

    feat: expose findCommand method

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import parser from './parser';
+import findCommand from './command/find-command';
 import autocompleteArgumentValue from './argument/autocomplete-argument-value';
 import { Command, CommandArgument, ParsedArgument } from './models/command.model';
 
-export { parser, autocompleteArgumentValue, Command, CommandArgument, ParsedArgument };
+export { parser, autocompleteArgumentValue, findCommand, Command, CommandArgument, ParsedArgument };
 export default parser;
