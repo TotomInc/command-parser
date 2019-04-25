17873c39f43e0a3f51eba983d337ab1f8600dfc3
Thomas Cazade <cazade.thomas@gmail.com>
2019-04-25 17:58:24 +0200

    chore(argument): find-argument documentation

--- src/argument/find-argument.ts
+++ src/argument/find-argument.ts
@@ -1,11 +1,11 @@
 import { Command, CommandArgument } from '../models/command.model';
 
 /**
- * Try to find an argument of a command with a value which can be an argument
- * name or value. Try to find both by argument name and alias.
+ * Try to find an argument of a command based on a value. Find both by argument
+ * name and alias.
  *
- * @param command original command to find argument
- * @param value name or value of a potential argument
+ * @param command command to find arguments against
+ * @param value argument name of argument value
  */
 function findArgument(command: Command, value: string): CommandArgument | undefined {
   let argument: CommandArgument | undefined;
